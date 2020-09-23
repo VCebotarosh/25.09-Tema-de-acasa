adabb6bb2f7ea130c7ee584d4004f02586cc4466
VCebotarosh <70701642+VCebotarosh@users.noreply.github.com>
2020-09-23 20:34:36 +0300

    Update prob_4.py

--- prob_4.py
+++ prob_4.py
@@ -1,5 +1,5 @@
 n=input("Dati un numar natural n= ")
 n=int(n)
 print(n,"metri =",n*100, "centimetri")
-print(n, "kilograme =",n*1000, "miligrame")
+print(n, "kilograme =",n*1000000, "miligrame")
 print(n,"ani =",n*12,"luni sau" ,n*365, "zile sau",n*52,"saptamiini")
